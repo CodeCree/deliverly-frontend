335c076fac868e8751679bd6ed1f9f7b7cd38de4
Josh Heng <josh@joshheng.co.uk>
2020-07-27 21:56:28 +0100

    Move operator app to operator directory and create track page

---
+++ operator/src/Map.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import { Segment } from 'semantic-ui-react';
+import ReactMapGL from 'react-map-gl';
+
+function Map() {
+	return (
+		<Segment>
+			<ReactMapGL />
+		</Segment>
+	);
+}
+
+export default Map;
