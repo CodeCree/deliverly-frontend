c5b760686ff888693cd079d3fed19620f5b8ccff
Josh Heng <josh@joshheng.co.uk>
2020-07-31 18:58:57 +0100

    Update user & warehouse management

--- src/Components/Map.js
+++ src/Components/Map.js
@@ -6,32 +6,36 @@ function Map(props) {
 	const [ map, setMap ] = useState(null);
 	const ref = createRef();
 	const { items } = props;
+	const [ markers, setMarkers ] = useState([]);
 
 	useEffect(() => {
 		if (!items) return;
 		if (items.length === 0) return;
-		mapboxgl.accessToken = process.env.REACT_APP_MAPBOX_ACCESS_TOKEN;
-		let _map = new mapboxgl.Map({
-			container: ref.current,
-			style: 'mapbox://styles/mapbox/streets-v11',
-			center: [items[0].position[1], items[0].position[0]],
-			zoom: 9
+
+		let _map = map;
+		if (!_map) {
+			mapboxgl.accessToken = process.env.REACT_APP_MAPBOX_ACCESS_TOKEN;
+			_map = new mapboxgl.Map({
+				container: ref.current,
+				style: 'mapbox://styles/mapbox/streets-v11',
+				center: [items[0].position[1], items[0].position[0]],
+				zoom: 9
+			});
+			setMap(_map);
+		}
+
+		markers.forEach(marker => {
+			marker.remove();
 		});
-		setMap(_map);
-	}, []);
-
-	useEffect(() => {
-		if (!items) return;
-		if (items.length === 0) return;
-		if (!map) return;
 
 		let minPosition = [items[0].position[1], items[0].position[0]];
 		let maxPosition = [items[0].position[1], items[0].position[0]];
+
+		let _markers = [];
 		items.forEach((item) => {
-			new mapboxgl.Marker()
+			_markers.push(new mapboxgl.Marker()
 			.setLngLat([item.position[1], item.position[0]])
-			.addTo(map);
-			//console.log([item.position[1], item.position[0]]);
+			.addTo(_map));
 
 			if (item.position[1] < minPosition[0]) minPosition[0] = item.position[1];
 			else if (item.position[1] > maxPosition[0]) maxPosition[0] = item.position[1];
@@ -44,11 +48,14 @@ function Map(props) {
 		maxPosition[0] = maxPosition[0] + 0.02;
 		maxPosition[1] = maxPosition[1] + 0.02;
 		
-		map.fitBounds([minPosition, maxPosition]);
-	}, [map, items]);
+		_map.fitBounds([minPosition, maxPosition]);
+
+		setMarkers(_markers);
+		// eslint-disable-next-line react-hooks/exhaustive-deps
+	}, [items]);
 
 	return (
-		<Segment style={{padding: '0.7rem 0.7rem 2rem', margin: '1rem 0'}}>
+		<Segment style={{padding: '0.7rem 0.7rem 2rem', margin: '1rem 0', display: ((!items) || items.length === 0) && 'none'}}>
 			<div ref={ref} style={{height: '30rem'}} />
 		</Segment>
 	);
